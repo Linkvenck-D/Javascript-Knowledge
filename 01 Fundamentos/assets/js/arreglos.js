9f9a7a780682a4e59f1610d0a23a441ca194376b
Linkvenck <linkvenck@gmail.com>
2021-08-01 01:15:58 -0500

    how to create an array, knowing properties and how to access them

---
+++ 01 Fundamentos/assets/js/arreglos.js
@@ -0,0 +1,34 @@
+
+// Forma 1 de crear un arreglo
+const arr=new Array(10);
+console.log(arr);
+
+// Forma 2 de crear un arreglo
+const arr2 =[];
+
+//forma 3 de crear un arreglo
+let videojuegos = ["mario 3", "Megaman", "Chrono Trigger"];
+console.log(videojuegos);
+console.log(videojuegos[0]);
+
+let arregloCosas = [
+    true,
+    123,
+    "Draven",
+    1+2,
+    function(){},
+    ()=>{},
+    {a: 1},
+    ["X", "MegaMan", "Zero", [
+        "MonkaS", "Pepega",
+    ]]
+];
+
+console.log(arregloCosas);
+console.log(arregloCosas[0]);
+console.log(arregloCosas[7][2]);
+console.log(arregloCosas[7][3][0]);
+
+
+
+
