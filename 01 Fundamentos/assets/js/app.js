52de1a79559a1957d697b901df62af60172db3fb
Linkvenck <linkvenck@gmail.com>
2021-08-01 00:44:05 -0500

    .

---
+++ 01 Fundamentos/assets/js/app.js
@@ -0,0 +1,7 @@
+alert("hola desde app.js");
+//console.log("hola mundo");
+let a = 10;
+let b = 10;
+let c = 10;
+console.info(a);
+console.table({a,b,c});
---
+++ 01 Fundamentos/assets/js/primitivos.js
@@ -0,0 +1,32 @@
+//Declarando variable tipo string
+let nombre = "Peter parker";
+console.log(nombre);
+
+nombre = "Ben parker";
+console.log(nombre);
+
+//mMostrando el tipo de dato
+console.log(typeof nombre); 
+
+
+//Mutando el tipo de dato de string a number asignando numeros
+// a la variable que anteriormente era de tipo string.
+// Javascript tiende a asignar tipos de datos de manera automatica
+// dependiendo el tipo de dato que se le esté asignando
+// para querer tener tipos fijos se recomienda el uso de TypeScript
+nombre= 123
+console.log(typeof nombre); 
+
+//Declarando variable tipo boolean
+let esMarvel=true;
+console.log(typeof esMarvel);
+
+//Declarando variable tipo number
+let edad = 33;
+console.log(typeof edad);
+
+edad=33.01;
+console.log(typeof edad);
+
+//Usando camelCase que es definir el nombre de la variable que 
+// estén conformadas por distintas palabras 
